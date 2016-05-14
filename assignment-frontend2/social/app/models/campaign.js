ca0a3e98e0bb50cb7f25aef53b4b42a7f368ee40
Stefanie Habersatter <shabersatter.mmt-b2014@fh-salzburg.ac.at>
2016-05-14 20:43:18 +0200

    Modified Models

--- assignment-frontend2/social/app/models/campaign.js
+++ assignment-frontend2/social/app/models/campaign.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data'
 
 export default Model.extend({
-  name: DS.attribute('string')
+  name: DS.attribute('string'),
   projects: DS.hasMany('projects')
 });
--- assignment-frontend2/social/app/models/project.js
+++ assignment-frontend2/social/app/models/project.js
@@ -2,7 +2,7 @@
 import DS from 'ember-data'
 
 export default Model.extend({
-  name: DS.attribute('string')
+  name: DS.attribute('string'),
   currentCampaign: DS.belongsTo('campaign'),// One-to-One
   campaigns: DS.hasMany('campaign')// One-to-Many or Many-to-Many
 });
